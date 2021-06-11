6df2f16d403e8b5df9ba6661b6c7509991117cb8
WrightRocket <keith.wright.rocket@gmail.com>
2021-06-10 20:56:05 -0700

    updated script.sh for show

--- script.sh
+++ script.sh
@@ -4,3 +4,4 @@
 echo "Hello, $1" # print Hello with the first word after the script name
 echo "You too: $2" # $2 would be the second word after the script name
 echo  "Brought to you by $0" # $0 refers to the script name
+echo "Test for backup ending in ~"
